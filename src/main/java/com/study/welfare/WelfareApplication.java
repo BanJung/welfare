d3b71c0acb5e924e10e8ac45c539665120970dcf
BanJung <higkdl813@gmail.com>
2025-02-12 12:40:19 +0900

    feat: Initialize project

---
+++ src/main/java/com/study/welfare/WelfareApplication.java
@@ -0,0 +1,13 @@
+package com.study.welfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelfareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelfareApplication.class, args);
+    }
+
+}
